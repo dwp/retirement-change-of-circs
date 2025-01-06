0474dcfcba076c5fad90eb69389589961ac50737
anilkorotania <152495384+anilkorotania@users.noreply.github.com>
2025-01-06 09:42:49 +0000

    OTFD agent experience updates

--- app/routes.js
+++ app/routes.js
@@ -2756,6 +2756,27 @@ router.post('/manual-address-2', function (req, res) {
       }
     
     });
+
+    // ROUTING TO MATCHING ADDRESS OR MANUAL ENTRY
+    router.post('/pscs-agent-action-1', function (req, res) {
+
+      // Make a variable and give it the value from 'bank-or-build'
+      var selectPscsAgentAction = req.session.data['pscs-agent-action']
+      console.log("high", req.session.data['pscs-agent-action'])
+    
+      // Check whether the variable matches a condition
+      if (selectPscsAgentAction == "Yes"){
+        // Send user to next page
+        res.redirect('/otfd-agent/2-0/task_list_confirm.html')
+      } else if (selectPscsAgentAction == "No"){
+        // Send user to next page
+        res.redirect('/otfd-agent/2-0/task_list_transferred.html')
+      }
+    
+    });
+
+        
+
   
 
 module.exports = router
